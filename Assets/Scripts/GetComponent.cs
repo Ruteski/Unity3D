3f9a2ef255eb75c0b293cf19406028314b0e4fd3
Lincoln Ruteski <lruteski@gmail.com>
2022-05-04 11:57:40 -0300

    Manipulando GetComponents e GameObjects

--- Assets/Scripts/GetComponent.cs
+++ Assets/Scripts/GetComponent.cs
@@ -7,11 +7,22 @@ public class GetComponent : MonoBehaviour
     private Rigidbody rb;
     private BoxCollider collider;
 
+    public GameObject obj;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         collider = GetComponent<BoxCollider>();
+
+        //obj.SetActive(false);
+        //obj.GetComponent<Light>().enabled = false;
+
+        if (obj.activeSelf) {
+            obj.SetActive(false);
+        } else {
+            obj.SetActive(true);
+        }
     }
 
     // Update is called once per frame
