6d9852f339652b0e2797399809797f66a34ab6ee
Lincoln Ruteski <lruteski@gmail.com>
2022-05-04 09:34:05 -0300

    script e variaveis

---
+++ Assets/Scripts/Script.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script : MonoBehaviour
+{
+    public int ValorInteiro;
+    private int ValorInt;
+    int Valor = 10;
+
+    public float Numerosagua = 25.6f;
+    string Texto = "uma string";
+
+    public GameObject go;
+    public Transform trf;
+    public Rigidbody body;
+    public Collider collider;
+
+
+
+}
